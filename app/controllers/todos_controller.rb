d15e2b03b7cb30101c49a95c0c29a4a161bc94e5
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-08 12:44:18 +0100

    add create method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,15 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todo.all.order(:created_at)    
+    @todos = Todo.all  
+    @todo = Todo.new 
+  end
+
+  def create
+    @todo = Todo.create(description: params[:todo][:description])
+    if @todo.valid?
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 end
